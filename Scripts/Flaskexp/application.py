064aaf1549f962ca621815db8462d58afa5dd89e
Aditya <adirosgaz@gmail.com>
2019-11-01 20:23:25 +0530

    Updated File

--- Scripts/Flaskexp/application.py
+++ Scripts/Flaskexp/application.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 
 def index():
     return "Hello World!"
+
+@app.route("/aditya")
+
+def aditya():
+    return "Hello Aditya!"
+
+@app.route("/<string:name>")
+
+def adi(name):
+    name = name.capitalize()
+    return f"<h1>Hello {name}}!</h1>"
