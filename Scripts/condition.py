723b4f20bdd9ddc88eef39ad89046266b4caf83a
Aditya <adirosgaz@gmail.com>
2019-11-01 19:00:09 +0530

    Sample code of Varialbe and Conditions

---
+++ Scripts/condition.py
@@ -0,0 +1,7 @@
+x=int(input("Enter a number")) # Converts input string to Integer
+if x>0:
+    print(f"{x} is Positive")
+elif x<0:
+    print(f"{x} is Negetive")
+else:
+    print("It is zero")
---
+++ Scripts/variables.py
@@ -0,0 +1,8 @@
+i=3
+print(f"\n i is {i} \n")
+f=3.3
+print(f"\n f is {f} \n")
+b=True
+print(f"\n b is {b} \n")
+n=None
+print(f"\n n is {n} \n")
